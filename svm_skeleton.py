be6feee8656a2887d8c722e807a082a010dc9f7e
Martin Biroščák <martin.biroscak@gmail.com>
2019-01-26 13:58:11 +0100

    Remove old code

--- svm_skeleton.py
+++ svm_skeleton.py
@@ -8,39 +8,21 @@ import cvxopt.solvers
 
 import matplotlib.pyplot as plt
 
-
-def buildHessian(X, y):
-    # Gram matrix
-    K = np.dot(X, X.T)
-    K = cvxopt.matrix(K)
-
-    # Hessian matrix
-    H = np.outer(y, y.T) * K
-    H = cvxopt.matrix(H)
-    return H
-
-
-def buildHessian2(X, y):
-    Y = np.diag(y)
-    H = np.dot(np.dot(np.dot(Y, X), X.T), Y)
-    H = cvxopt.matrix(H)
-    return H
-
-
 class SVM(object):
     lagrange_multipliers = []
     w = np.array([])
     b = 0
 
-    def buildHessian(X, y):
+    def buildHessian(self, X, y):
         Y = np.diag(y)
-        H = np.dot(np.dot(np.dot(Y, X), X.T), Y);
+        H = np.dot(np.dot(np.dot(Y, X), X.T), Y)
+        H = cvxopt.matrix(H)
+        return H
 
     def train(self, X, y):
         n_samples, n_features = X.shape
 
-        P = buildHessian(X, y)
-        # P = buildHessian2(X, y)
+        P = self.buildHessian(X, y)
 
         # RHS
         q = cvxopt.matrix(np.ones(n_samples) * -1)
@@ -65,11 +47,6 @@ class SVM(object):
 
     def predict(self, X):
         return X @ self.w - self.b
-
-    def reconstructOld(self, X, y):
-        sv = self.lagrange_multipliers > 1e-5
-        self.w = np.sum([self.lagrange_multipliers[i] * y[i] * X[i] for i in range(0, len(X))], axis=0)
-        self.b = np.sum([np.dot(X[i], self.w) - y[i] for i, x in enumerate(sv) if x]) / sum(sv)
 
     def reconstruct(self, X, y):
         self.w = np.asarray(y @ np.diag(self.lagrange_multipliers) @ np.matrix(X)).flatten()
